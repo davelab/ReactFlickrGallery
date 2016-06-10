f3d6c36c6907c190373572f68ae2037ffb965c06
Davide Cariani <davide.cariani@engrande.com>
2016-06-10 17:07:53 +0200

    responsivness

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
 const key =  'cce3984558b3d93c7f8c9404b649584e';
-const endpoint = 'https://api.flickr.com/services/rest/?api_key='+key+'&format=rest&format=json&nojsoncallback=1';
+const endpoint = `https://api.flickr.com/services/rest/?api_key=${key}&format=rest&format=json&nojsoncallback=1`;
 
 export default endpoint;
